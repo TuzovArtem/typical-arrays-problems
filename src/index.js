e45aaab90383354cec31c46e3395670d865bffa8
Artem_T <tuzovartem@gmail.com>
2020-10-02 22:39:06 +0400

    feat: add solution "max","min, "avg"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-
 exports.min = function min (array) {
-  return 0;
-}
+  console.log(array);
+  if( typeof(array) === 'undefined' || array[0] === undefined){
+    console.log('yes')
+    return 0;
+  }else {
+    let min = 0;
+    for(let i = 0; i<array.length; i++){
+      if(array[i] < min){
+        min=array[i];
+      }
+    }
+    return min;
+  }
+};
 
 exports.max = function max (array) {
-  return 0;
+
+  if(typeof(array) === 'undefined' || array[0] === undefined){
+    return 0;
+  }else {
+  let max = 0;
+  for(let i = 0; i<array.length; i++){
+    if(array[i] > max){
+      max=array[i];
+    }
+  }
+  return max;
 }
+};
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof(array) === 'undefined' || array[0] === undefined){
+    return 0;
+  }else {
+  let sum = 0;
+  array.forEach(function(element) { sum+=element });
+      
+ return sum/array.length;
 }
+};
